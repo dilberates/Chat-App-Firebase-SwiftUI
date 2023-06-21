381853e240ddc48392a7a7087d21d29c0d83be36
Dilber KILIÇ <48033000+dilberkilic@users.noreply.github.com>
2023-06-22 00:56:30 +0300

    Add master
    Add master

---
+++ Chat-App-SwiftUI/Chat-App-SwiftUI/Chat_App_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chat_App_SwiftUIApp.swift
+//  Chat-App-SwiftUI
+//
+//  Created by Dilber KILIÇ on 22.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct Chat_App_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

e8d49cedbceaf7ad9cba2bfef61d857a095c2bf3
Dilber KILIÇ <48033000+dilberkilic@users.noreply.github.com>
2023-06-22 21:55:57 +0300

    Add textfield, button
    Add textfield, button

--- Chat-App-SwiftUI/Chat-App-SwiftUI/ContentView.swift
+++ Chat-App-SwiftUI/Chat-App-SwiftUI/ContentView.swift
@@ -10,18 +10,49 @@ import SwiftUI
 struct ContentView: View {
     
     @State var isLoginMode=false
+    @State var email=""
+    @State var password=""
     
     var body: some View {
         NavigationView{
             ScrollView{
-                Picker(selection: $isLoginMode, label: Text("picker here")){
-                    Text("Login")
-                    Text("Create Account")
-                }.pickerStyle(SegmentedPickerStyle()).padding()
-                Text("HERE ITS ME")
-            }.navigationTitle("Create Account")
-        }
-            Text("Login Page").padding()
+                
+                VStack{
+                    Picker(selection: $isLoginMode, label: Text("picker here")){
+                        Text("Login")
+                            .tag(true)
+                        Text("Create Account")
+                            .tag(false)
+                    }.pickerStyle(SegmentedPickerStyle()).padding()
+                    
+                    Button {
+                        
+                    }label: {
+                        Image(systemName: "person.fill")
+                            .font(.system(size: 70))
+                            .padding()
+                    }
+                    
+                    TextField( "Email",text: $email)
+                    TextField("Password",text: $password)
+                    
+                    Button{}label: {
+                        HStack{
+                            Text("Create Account")
+                                .foregroundColor(.white)
+                                .padding(.vertical,10)
+                            Spacer()
+                            
+                        }.background(Color.blue)
+                    }
+                    
+                    Text("HERE ITS ME")
+                }.navigationTitle("Create Account")
+            }
+                Text("Login Page").padding()
+        }.padding()
+                
+                
     }
 }
 
